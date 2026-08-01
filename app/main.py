6e15c3feeeaac6ab421ee0dbfac2ddcd593ee370
AmnaAli45 <anasahmed.ru6@gmail.com>
2026-08-01 23:26:38 +0500

    route connected to main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from app.database import engine, Base
-from app.models import db_models   # ye line zaroori hai
+from app.models import db_models
+from app.routers import track
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="GetScry API")
+
+app.include_router(track.router)   # ye naya line add karein
 
 @app.get("/")
 def home():
